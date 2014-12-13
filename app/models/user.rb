c2b77da8efda6da65a0c741d9a0f13cffeea7848
Jacob <jacob.millner@outlook.com>
2014-12-13 14:31:16 -0500

    create user resource

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+	attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
+	has_secure_pasword
+	before_validation :prep_email
+	before_save :create_avatar_url
+	validates :name, presence: true
+	validates :username, uniqueness: true, presence: true
+	validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+	
+	private
+ 
+	def prep_email
+    		self.email = self.email.strip.downcase if self.email
+	end
+	
+	def create_avatar_url
+		self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+	end
+end
