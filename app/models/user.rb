ed9b353077475853a97669437333d65f86efe8c3
Jacob <jacob.millner@outlook.com>
2014-12-15 20:28:43 -0500

    fixed some syntax errors and removed debug code

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
 	validates :username, uniqueness: true, presence: true
 	validates :email, uniqueness: true, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 	has_many :posts
-	has_many :follower_relationships, classname: "Relationship", foreign_key "followed_id"
-	has_many :followed_relationships, classname: "Relationship", foreign_key "follower_id"
+	has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id"
+	has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id"
 	has_many :followers, through: :follower_relationships
 	has_many :followeds, through: :followed_relationships
  	
