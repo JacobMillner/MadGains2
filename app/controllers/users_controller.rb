4f2e414e8d5846ff43cad2447e60dcbbe5e6a937
Jacob <jacob.millner@outlook.com>
2014-12-15 21:01:36 -0500

    Wooo, follow/unfollow now working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@post = Post.new
+
+		@relationship = Relationship.where(
+    			follower_id: current_user.id,
+    			followed_id: @user.id
+		).first_or_initialize if current_user
 	end
 
 	def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,14 @@ class User < ActiveRecord::Base
 	has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id"
 	has_many :followers, through: :follower_relationships
 	has_many :followeds, through: :followed_relationships
+
+	def following? user
+		self.followeds.include? user
+	end
+
+	def follow user
+		Relationship.create follower_id: self.id, followed_id: user.id
+	end
  	
 	private
  
@@ -22,11 +30,4 @@ class User < ActiveRecord::Base
 		self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
 	end
 
-	def following? user
-		self.followeds.include? user
-	end
-
-	def follow user
-		Relationship.create follower_id: self.id, followed_id: user.id
-	end
 end
