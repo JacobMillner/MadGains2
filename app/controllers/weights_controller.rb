d8f7f88a6602ad0e3404acd2f96a4135d807922b
Jacob <jacob.millner@outlook.com>
2014-12-21 16:30:43 -0500

    Added profile edit page, finished weights controller

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,2 +1,21 @@
 class WeightsController < ApplicationController
+
+    def weight_params
+    	params.require(:weight).permit(:content, :user_id) 
+    end	   
+
+
+    def create
+   	@weight = Weight.new(weight_params)
+   	@weight.user_id = current_user.id
+    
+   	if @weight.save
+        	redirect_to current_user
+        else
+        	flash[:error] = "Problem!"
+        	redirect_to current_user
+     	end
+    end
+
+
 end
