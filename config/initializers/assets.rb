544ad9d1f6c4923383544643b9de64ca13ba21b9
Jacob <jacob.millner@outlook.com>
2015-02-08 18:04:49 -0500

    One step closer to getting the theme set up!

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.0' 
 Rails.application.config.assets.precompile += %w( sandstone.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( plugins.css )
+Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( icons.css )
 Rails.application.config.assets.precompile += %w( sandstone.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
